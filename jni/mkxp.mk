974283ab91589eb9697a39a8f61b77d26968e992
Varun Patil <radialapps@gmail.com>
2023-04-22 12:19:40 -0700

    Make sure sdl is static

--- jni/mkxp.mk
+++ jni/mkxp.mk
@@ -93,7 +93,7 @@ LOCAL_SRC_FILES := \
 	$(LOCAL_PATH)/binding-mri/windowvx-binding.cpp \
 	$(LOCAL_PATH)/binding-mri/tilemapvx-binding.cpp \
 
-LOCAL_STATIC_LIBRARIES:=vorbis physfs sigc++ pixman ruby SDL2_static SDL2_ttf SDL2_sound SDL2_image ogg
+LOCAL_STATIC_LIBRARIES:=vorbis physfs sigc++ pixman ruby SDL2_static SDL2_ttf_static SDL2_sound_static SDL2_image_static ogg
 LOCAL_SHARED_LIBRARIES:=openal mkxp_wrapper fluidsynth
 LOCAL_LDLIBS:=-lz -llog -ldl -lm -lOpenSLES
 include $(BUILD_SHARED_LIBRARY)
